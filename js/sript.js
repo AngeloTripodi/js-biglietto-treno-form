50c69abdbcd8a47be76a228d3f1527f3920d694d
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-22 17:40:39 +0100

    Setup Milestone 1

---
+++ js/sript.js
@@ -0,0 +1,9 @@
+const inputKm = document.getElementById('km');
+const inputAge = document.getElementById('travelerAge');
+
+const button = document.getElementById('my-btn');
+button.addEventListener('click', function(){
+    console.log(km.value);
+    console.log(inputAge.value);
+});
+
